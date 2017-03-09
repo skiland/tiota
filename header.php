3b048e965640d06a7f26c1511caf7abbc07fcc7a
Nicolas Heine <nicolas.heine11@gmail.com>
2017-03-09 13:30:59 +0100

    Modif header

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
 		<link href="css/main.css" media="screen, projection" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="library/font-awesome/css/font-awesome.min.css">
 	</head>
---
+++ notif.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+
+<header>
+    <h2><a href="javascript:void(0)">Profil</a></h2>
+    <h1><a href="javascript:void(0)">Notification</a></h1>
+    <h2><a href="javascript:void(0)">Accueil</a></h2>
+</header>
+
+
+<?php include('footer.php');
