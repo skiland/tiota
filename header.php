e297533bc309a7f220e7099f7d69a81e3c2270aa
Nicolas Heine <nicolas.heine11@gmail.com>
2017-03-09 13:22:52 +0100

    Ajout font awesome

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
-	  <link href="/css/main.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<link href="css/main.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="library/font-awesome/css/font-awesome.min.css">
 	</head>
 	<body>
